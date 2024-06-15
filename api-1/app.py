2c0ae05d0bd6c2cdbc5f3fe6596ccc029237ff5f
Abraham <kashuaabraham@yahoo.com>
2024-06-15 09:06:26 +0100

    Update app to return hello world message

--- api-1/app.py
+++ api-1/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return "API-1: Use /hello to get 'Hello World'", 200
+
 @app.route('/hello', methods=['GET'])
 def hello_world():
     return "Hello World", 200
