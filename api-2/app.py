99f44a6c89f7649eebe0c661f2d01a4d07112a5e
Abraham <kashuaabraham@yahoo.com>
2024-06-15 09:07:40 +0100

    update app to return message from api 1

--- api-2/app.py
+++ api-2/app.py
@@ -3,6 +3,10 @@ import requests
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return "API-2: Use /call-api-1 to get response from API-1", 200
+
 @app.route('/call-api-1', methods=['GET'])
 def call_api_1():
     response = requests.get('http://api-1:5000/hello')
